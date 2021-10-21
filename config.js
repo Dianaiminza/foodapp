87b788c652da651bbfc301b2876cb9f46a1a9898
DIANA IMINZA <51244262+Dianaiminza@users.noreply.github.com>
2021-10-21 18:45:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ dotenv.config();
 
 module.exports={
  PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://dbUser:Captain@cluster0.w11hf.mongodb.net/food?retryWrites=true&w=majority',
+  MONGODB_URL: process.env.MONGODB_URL || '',
   JWT_SECRET: process.env.JWT_SECRET || 'captain',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
-  GOOGLE_API_KEY:process.env.GOOGLE_API_KEY || 'AIzaSyA_o7FM2m9ZPRceHDT9EDgrVtFIslIQHQ8',
+  GOOGLE_API_KEY:process.env.GOOGLE_API_KEY || '',
   accessKeyId: process.env.accessKeyId || 'accessKeyId',
   secretAccessKey: process.env.secretAccessKey || 'secretAccessKey',
 
